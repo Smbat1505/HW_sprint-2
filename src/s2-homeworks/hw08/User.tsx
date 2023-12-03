badcbdc3cba3e97f18ff2205961a0447d5455403
Smbat1505 <1cor13a8@gmail.com>
2023-12-03 20:47:57 +0400

    Did my homework HW8, HW9

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -8,17 +8,20 @@ type UserPropsType = {
 }
 
 const User: React.FC<UserPropsType> = ({ u }) => {
-    return (
-        <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
-            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
-                {/*отобразить имя*/}
+    return (<>
 
-            </td>
-            <td id={'hw8-user-age-' + u._id}>
-                {/*отобразить возраст*/}
 
-            </td>
-        </tr>
+            <tr id={`hw8-user-${u._id}-${u.age}`} className={s.item}>
+
+                <td id={`hw8-user-name-${u._id}`} className={s.nameCol}>
+                    {u.name}
+
+                </td>
+                <td id={'hw8-user-age-' + u._id}>
+                    {u.age}
+
+                </td>
+            </tr></>
     )
 }
 
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let sorted = [...state];
+            sorted = state.sort((a, b) => a.name.localeCompare(b.name))
 
-            return state // need to fix
+            if (action.payload === 'down') {
+                return sorted.reverse()
+            }
+            return sorted
         }
         case 'check': {
-
-            return state // need to fix
+            let check = [...state];
+            return check.filter(a => a.age >= 18)
         }
         default:
             return state
